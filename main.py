4587ee3a6a0ce287180c115d06c2d43bee20db8b
numbers1234567 <33708611+numbers1234567@users.noreply.github.com>
2024-03-31 23:51:00 -0500

    Remove async

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, List
+from pydantic import BaseModel
+
+class PostInfo(BaseModel):
+    post_id : str
+    text : str
+    media_urls : list[str] = []
+
 
 app = FastAPI()
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id : int):
-    return {"item_id": item_id}
+@app.post("/process_post/{user_key}")
+def process_post(user_key : str, post_info : PostInfo):
+    return {"message" : "bruh"}
+
+@app.post("/blocked_post/{user_key}")
+def block_post(user_key : str, id : str):
+    return 

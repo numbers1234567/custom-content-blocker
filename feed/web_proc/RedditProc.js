97a9b07187ce85dd0c0e94418c636f4b6b36dc0e
numbers1234567 <33708611+numbers1234567@users.noreply.github.com>
2024-04-07 03:14:17 -0500

    Rework site processing

--- feed/web_proc/RedditProc.js
+++ feed/web_proc/RedditProc.js
@@ -1,6 +1,9 @@
 
 class RedditProc extends WebProc {
     className = "Post";
+    baseRegEx     = new RegExp(".*://i\.redd\.it/*");
+    previewRegEx  = new RegExp(".*://preview\.redd\.it/*");
+    postLinkRegEx = new RegExp("/r/.*/comments/.*/.*/")
     getPostFromChildEl(el) {
         while (el && !el.className.split(" ").includes(this.className)) 
             el = el.parentElement;
@@ -10,19 +13,15 @@ class RedditProc extends WebProc {
     getImgFromPreviewURL(url) {
         // Returned form: https://i.redd.it/[filename]
         // list of matching expressions
-        if (new RegExp(".*://i\.redd\.it/*").test(url)) return url;
-        if (new RegExp(".*://preview\.redd\.it/*").test(url)) {
+        if (this.baseRegEx.test(url)) return url;
+        if (this.previewRegEx.test(url)) {
             let qName = url.split("it/")[1];
             let filename = qName.split("?")[0];
             return "https://i.redd.it/" + filename;
         }
         return null;
     }
-    getRelevantData(el) {
-        el = this.getPostFromChildEl(el);
-        // Get title
-        let title = el.querySelector("div[data-adclicklocation=title]").children[0].textContent;
-        // Get images
+    getImgUrls(el) {
         let imgEls = el.getElementsByTagName("img");
         var imgLinks = []
         for (const imgEl of imgEls) {
@@ -30,9 +29,25 @@ class RedditProc extends WebProc {
             if (imgLink)
                 imgLinks.push(imgLink);
         }
-        // potentially getting other media here
-        // ...
-
-        return {text : title, media: {images: imgLinks}};
+        return imgLinks;
+    }
+    getVideoUrls(el) {
+        
+    }
+    getText(el) {
+        return el.querySelector("div[data-adclicklocation=title]").children[0].textContent;
+    }
+    getID(el) {
+        let anchors = el.getElementsByTagName("a");
+        for (const anchor of anchors) {
+            let link = anchor.getAttribute("href");
+            if (this.postLinkRegEx.test(link))
+                return link.replace("/comments", "").replaceAll("/", "_");
+        }
+        return null;
+    }
+    getRelevantData(el) {
+        el = this.getPostFromChildEl(el);
+        return super.getRelevantData(el);
     }
 }
--- feed/web_proc/WebProc.js
+++ feed/web_proc/WebProc.js
@@ -5,7 +5,20 @@ class WebProc {
     getPostFromChildEl(el) {
         throw new WebProcUndefinedError("Processing undefined for this website");
     }
-    getRelevantData(el) {
+    getImgUrls(el) {
         throw new WebProcUndefinedError("Processing undefined for this website");
+    }
+    getVideoUrls(el) {
+        throw new WebProcUndefinedError("Processing undefined for this website");
+    }
+    getText(el) {
+        throw new WebProcUndefinedError("Processing undefined for this website");
+    }
+    getID(el) {
+        throw new WebProcUndefinedError("Processing undefined for this website");
+    }
+    getRelevantData(el) {
+        return {media: {text: this.getText(el), images: this.getImgUrls(el), video: this.getVideoUrls(el)}, 
+                metadata: {id: this.getID(el)}};
     }
 };

e8fb160c0ffd4f35cafcdcf40ec06388224fd053
Angelo <angelochem4@gmail.com>
2024-04-12 14:26:15 -0500

    Switch to Fetch API

--- feed/filter_api_target.js
+++ feed/filter_api_target.js
@@ -2,14 +2,24 @@ const FILTER_API_URL = "http://127.0.0.1:8000";//"[API URL here]";
 var current_key = "admin";
 
 function filter_post(post_data) {
-    const xhr = new XMLHttpRequest();
-    xhr.open("POST", `${FILTER_API_URL}/process_post/${current_key}/`);
-    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    const body = JSON.stringify(post_data);
-    console.log(body);
-    xhr.onload = () => {
-        console.log("Success");
-    }
-    xhr.send(body);
-    console.log(`${FILTER_API_URL}/process_post/${current_key}/`);
+    
+    // Send the POST request using fetch
+    fetch(`${FILTER_API_URL}/process_post/${current_key}/`, {
+        method: "POST",
+        mode: "no-cors",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(post_data)
+    }).then(
+        (data) => {
+            console.log(data);
+        }
+    ).catch(
+        (error) => {
+            console.log(error);
+        }
+    );
+    //.then((response) => response.json())
+    //.then((data) => console.log("Success:", data));
 }

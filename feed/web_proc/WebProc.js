37d63e20485de38814e65a985b4b8ce74b3ee89a
numbers1234567 <33708611+numbers1234567@users.noreply.github.com>
2024-04-27 18:19:41 -0500

    Rework to send raw data through the JSON file.

--- feed/web_proc/WebProc.js
+++ feed/web_proc/WebProc.js
@@ -6,21 +6,21 @@ class WebProc {
     getPostFromChildEl(el) {
         throw new WebProcUndefinedError("Processing undefined for this website");
     }
-    getImgUrls(el) {
+    getImgData(el) { // Return as a list of {content : [Raw Data as a string], dataFormat : string}
         throw new WebProcUndefinedError("Processing undefined for this website");
     }
-    getVideoUrls(el) {
+    getVidData(el) { // Return as a list of {content : [Raw Data as a string], dataFormat : string}
         throw new WebProcUndefinedError("Processing undefined for this website");
     }
-    getText(el) {
+    getText(el) { // Return as a string
         throw new WebProcUndefinedError("Processing undefined for this website");
     }
-    getID(el) {
+    getID(el) { // Return as a string
         throw new WebProcUndefinedError("Processing undefined for this website");
     }
     getRelevantData(el) { // Get some data from a post. Shouldn't need to overload this.
         el = this.getPostFromChildEl(el);
-        return {media: {text: this.getText(el), images: this.getImgUrls(el), video: this.getVideoUrls(el)}, 
+        return {media: {text: this.getText(el), images: this.getImgData(el), video: this.getVidData(el)}, 
         metadata: {id: this.getID(el)}};
     }
     // Data node where posts are dynamically loaded

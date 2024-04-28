af6b6c79510dc835f4864bc3c6b383371462d578
numbers1234567 <33708611+numbers1234567@users.noreply.github.com>
2024-04-28 18:31:17 -0500

    Sensible return value now that I understand Promises better.

--- feed/web_proc/WebProc.js
+++ feed/web_proc/WebProc.js
@@ -6,10 +6,10 @@ class WebProc {
     getPostFromChildEl(el) {
         throw new WebProcUndefinedError("Processing undefined for this website");
     }
-    getImgData(el) { // Return as a list of {content : [Promise of Raw Data as a string], dataFormat : string}
+    getImgData(el) { // Return as a list of promises as {content : string, dataFormat : string}
         throw new WebProcUndefinedError("Processing undefined for this website");
     }
-    getVidData(el) { // Return as a list of {content : [Promise of Raw Data as a string], dataFormat : string}
+    getVidData(el) { // Return as a list of promises as {content : string, dataFormat : string}
         throw new WebProcUndefinedError("Processing undefined for this website");
     }
     getText(el) { // Return as a string
@@ -18,10 +18,22 @@ class WebProc {
     getID(el) { // Return as a string
         throw new WebProcUndefinedError("Processing undefined for this website");
     }
-    getRelevantData(el) { // Get some data from a post. Shouldn't need to overload this.
+    getRelevantData(el) { // Get some data from a post. Return Promise of data since images and video are huge.
+        // Shouldn't need to overload this.
         el = this.getPostFromChildEl(el);
-        return {media: {text: this.getText(el), images: this.getImgData(el), video: this.getVidData(el)}, 
-        metadata: {id: this.getID(el)}};
+        let procObj = this;
+        return new Promise(function(resolve, reject) { // ew
+            Promise.all(procObj.getImgData(el)).then(
+            (imgData) => {
+                resolve({media: {text: procObj.getText(el), images: imgData, video: []}, 
+                    metadata: {id: procObj.getID(el)}});
+                /*Promise.all(procObj.getVidData(el)).then(
+                (vidData) => {
+                    resolve({media: {text: procObj.getText(el), images: imgData, video: vidData}, 
+                        metadata: {id: procObj.getID(el)}});
+                })*/
+            })
+        });
     }
     // Data node where posts are dynamically loaded
     getDataNode(el) {

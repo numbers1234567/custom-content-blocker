eed4682715e00412c43ddcd0b4fc9235189459d0
numbers1234567 <33708611+numbers1234567@users.noreply.github.com>
2024-07-29 21:12:17 -0400

    Added layer b/w user and db manager

---
+++ public-api/build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+python -m pip install -r requirements.txt
--- user-view/src/app/components/scroller.tsx
+++ user-view/src/app/components/scroller.tsx
@@ -10,12 +10,19 @@ export function PostBatch(
     const [htmlEmbed, setHtmlEmbed] = useState<string[]|null>(null);
     // Get HTML embeds for posts
     useEffect(() => {
-        let html = []
-        fetch(`${CURATE_API_PATH}/recent_posts?before=${beforeUTC}&count=10`)
+        let html = [];
+        console.log("Fetched! " + beforeUTC)
+        fetch(`${CURATE_API_PATH}/get_curated_posts`, 
+            {method: "POST",
+             headers: {
+                 'Content-Type': 'application/json',
+             },
+             body: JSON.stringify({username : "angelo", password : "pimienta", before : beforeUTC, curation_key : "half", count_min : 5, count_max : 10})
+            })
             .then(response => response.json())
             .then(json => {
-                html = json.html_embeds.map((item : {html : string, create_utc : number})=>item.html);
-                let create_utc = json.html_embeds.map((item : {html : string, create_utc : number})=>item.create_utc);
+                html = json.posts.map((item : {html : string, create_utc : number})=>item.html);
+                let create_utc = json.posts.map((item : {html : string, create_utc : number})=>item.create_utc);
                 // new earliest post
                 let earliest = create_utc[0];
                 for (const i of create_utc)  
@@ -27,22 +34,22 @@ export function PostBatch(
             })
             .catch(error => console.log(error));
     }, [beforeUTC]);
-    return <>
+    return <div>
         {htmlEmbed && 
             <>
-                <SocialPost embedStr={htmlEmbed[0]}/>
-                <SocialPost embedStr={htmlEmbed[1]}/>
-                <SocialPost embedStr={htmlEmbed[2]}/>
-                <SocialPost embedStr={htmlEmbed[3]}/>
-                <SocialPost embedStr={htmlEmbed[4]}/>
-                <SocialPost embedStr={htmlEmbed[5]}/>
-                <SocialPost embedStr={htmlEmbed[6]}/>
-                <SocialPost embedStr={htmlEmbed[7]}/>
-                <SocialPost embedStr={htmlEmbed[8]}/>
-                <SocialPost embedStr={htmlEmbed[9]}/>
+                {htmlEmbed.length >= 1  && <SocialPost embedStr={htmlEmbed[0]}/>}
+                {htmlEmbed.length >= 2  && <SocialPost embedStr={htmlEmbed[1]}/>}
+                {htmlEmbed.length >= 3  && <SocialPost embedStr={htmlEmbed[2]}/>}
+                {htmlEmbed.length >= 4  && <SocialPost embedStr={htmlEmbed[3]}/>}
+                {htmlEmbed.length >= 5  && <SocialPost embedStr={htmlEmbed[4]}/>}
+                {htmlEmbed.length >= 6  && <SocialPost embedStr={htmlEmbed[5]}/>}
+                {htmlEmbed.length >= 7  && <SocialPost embedStr={htmlEmbed[6]}/>}
+                {htmlEmbed.length >= 8  && <SocialPost embedStr={htmlEmbed[7]}/>}
+                {htmlEmbed.length >= 9  && <SocialPost embedStr={htmlEmbed[8]}/>}
+                {htmlEmbed.length >= 10 && <SocialPost embedStr={htmlEmbed[9]}/>}
             </>
         }
-    </>
+    </div>
 }
 
 export function PostScroller() {

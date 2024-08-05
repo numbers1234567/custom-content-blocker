70225a984eca9aaf8179367b2b8935913e2ca00e
numbers1234567 <33708611+numbers1234567@users.noreply.github.com>
2024-08-04 22:28:10 -0400

    BLIP features in db

--- postgres-db/test_dataset/init.sql
+++ postgres-db/test_dataset/init.sql
@@ -6,6 +6,12 @@ CREATE TABLE social_post_data (
     PRIMARY KEY (internal_id)
 );
 
+CREATE TABLE blip_features (
+    internal_id INT NOT NULL REFERENCES social_post_data(internal_id),
+    features DECIMAL[768] NOT NULL,
+    PRIMARY KEY (internal_id)
+);
+
 COPY social_post_data
 FROM '/docker-entrypoint-initdb.d/test_post_data.csv'
 DELIMITER E'\t'

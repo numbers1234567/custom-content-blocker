e87480dbf47caa9c5dd8d6bd88ff3ee4fcca218e
numbers1234567 <33708611+numbers1234567@users.noreply.github.com>
2024-08-10 02:16:29 -0400

    More organized and scalable request composition

--- user-view/src/app/api_call.tsx
+++ user-view/src/app/api_call.tsx
@@ -5,15 +5,19 @@
 
 import { CURATE_API_PATH } from "./config";
 import { Credentials } from "./credentials";
-import { CurationMode } from "./curation_settings";
+import { CurationMode, CurationSetting } from "./curation_settings";
 
 export type CuratePostsRequestBody = {
-  token : string,
-  before : number,
-  count_max : number,
-  count_min : number,
-  min_score : number,
-  curation_key : string,
+  credentials : {
+    token : string
+  }
+  curation_settings : CurationSetting
+  options : {
+    before : number,
+    count_max : number,
+    count_min : number,
+    min_score : number,
+  }
 }
 
 type CuratedPost = {
@@ -27,14 +31,26 @@ type CuratePostsResponseBody = {
   posts : CuratedPost[],
 }
 
-export async function getCuratedPosts(request : CuratePostsRequestBody) : Promise<CuratePostsResponseBody> {
+export async function getCuratedPosts(
+  credentials : Credentials, curation_settings : CurationSetting, 
+  beforeUTC : number, count_max : number = 10, count_min : number = 5, min_score : number = 0.5) : Promise<CuratePostsResponseBody> {
   let result : CuratePostsResponseBody = {posts : []};
+  const requestBody : CuratePostsRequestBody = {
+    credentials : credentials,
+    curation_settings : curation_settings,
+    options : {
+      before : beforeUTC, 
+      count_min : count_min, 
+      count_max : count_max,
+      min_score : min_score,
+    }
+  }
   await fetch(`${CURATE_API_PATH}/get_curated_posts`, 
     {method: "POST",
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(request)
+      body: JSON.stringify(requestBody)
     })
     .then(response => response.json())
     .then(json => { result = {...json}; })

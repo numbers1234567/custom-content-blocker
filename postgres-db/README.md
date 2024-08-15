44d8e1489fc7fd23bcb2fe723ebd0924567a0f71
numbers1234567 <33708611+numbers1234567@users.noreply.github.com>
2024-08-15 17:16:13 -0400

    BLIP features

--- postgres-db/README.md
+++
@@ -1,1 +0,0 @@
-https://medium.com/@asuarezaceves/initializing-a-postgresql-database-with-a-dataset-using-docker-compose-a-step-by-step-guide-3feebd5b1545
--- postgres-db/test_dataset/init.sql
+++ postgres-db/test_dataset/init.sql
@@ -13,7 +13,28 @@ CREATE TABLE blip_features (
     PRIMARY KEY (internal_id)
 );
 
+CREATE TABLE user_credentials (
+    user_id INT NOT NULL,
+    create_utc INT NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    PRIMARY KEY (user_id)
+);
+
+CREATE TABLE curation_modes (
+    primary_user INT NOT NULL REFERENCES user_credentials(user_id),
+    curation_name CHAR(20),
+    curation_key CHAR(40),
+    create_utc INT,
+    PRIMARY KEY (primary_user, curation_key)
+);
+
 COPY social_post_data
 FROM '/docker-entrypoint-initdb.d/test_post_data.csv'
 DELIMITER E'\t'
 CSV HEADER;
+
+COPY blip_features
+FROM '/docker-entrypoint-initdb.d/test_blip_data.csv'
+DELIMITER E'\t'
+CSV HEADER;
+

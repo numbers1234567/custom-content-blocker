a0e6a92913b3d50aecb1273ec86795457ce8f4e5
numbers1234567 <33708611+numbers1234567@users.noreply.github.com>
2024-08-16 17:46:06 -0400

    Curation modes in DB

--- postgres-db/test_dataset/init.sql
+++ postgres-db/test_dataset/init.sql
@@ -1,6 +1,6 @@
 CREATE TABLE social_post_data (
     internal_id INT NOT NULL,
-    post_id TEXT NOT NULL,
+    post_id TEXT NOT NULL UNIQUE,
     title TEXT NOT NULL,
     embed_html TEXT,
     create_utc INT,
@@ -16,16 +16,17 @@ CREATE TABLE blip_features (
 CREATE TABLE user_credentials (
     user_id INT NOT NULL,
     create_utc INT NOT NULL,
-    email VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
     PRIMARY KEY (user_id)
 );
 
 CREATE TABLE curation_modes (
     primary_user INT NOT NULL REFERENCES user_credentials(user_id),
-    curation_name CHAR(20),
-    curation_key CHAR(40),
+    curation_id INT NOT NULL,
+    curation_name VARCHAR(20),
+    curation_key CHAR(40) UNIQUE,
     create_utc INT,
-    PRIMARY KEY (primary_user, curation_key)
+    PRIMARY KEY (primary_user, curation_id)
 );
 
 COPY social_post_data
